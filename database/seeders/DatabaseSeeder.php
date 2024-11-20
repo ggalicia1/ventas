78a797a0d853e151a71fbcfa5c006f7eac54fc14
ggalicia1 <ggalicia@mintrabajo.gob.gt>
2024-11-19 23:31:21 -0600

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,12 +15,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(10)->create();
+        //User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::create([
+                        'name' => 'Gustavo Adolfo Galicia',
+                        'email' => 'gustavogalicia247@gmail.com',
+                        'email_verified_at' => now(),
+                        'password' => Hash::make('%#Nueva.12345#%'),
+                        'remember_token' => Str::random(10),
+                    ]);
+
+        User::create([
+                        'name' => 'Jeimy Andrea Ramirez Cano',
+                        'email' => 'canojeimycano@gmail.com',
+                        'email_verified_at' => now(),
+                        'password' => Hash::make('%#Nueva.12345#%'),
+                        'remember_token' => Str::random(10),
+                    ]);
 
         $this->call([
             CategorySeeder::class,
